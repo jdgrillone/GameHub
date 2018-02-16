aa5af9cbe92a77329f295324d5f00e14cfe00ce2
jdgrillone <jdgrillone@gmail.com>
2018-02-16 14:32:55 -0700

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+// eslint-disable-next-line
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import UserCreate from "./components/UserCreate";
+import Login from "./components/Login"
 import './App.css';
+import {
+  BrowseRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
