62e5237dbff1cc4ce6e4655f530e83e90839fee3
jdgrillone <jdgrillone@gmail.com>
2018-02-16 09:21:19 -0700

    Added depencencies for express server

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGO_URI || "mongod://localhost/gamehub",
+  {
+    useMongoClient: true
+  }
+);
 
+// Serve staic assets
+app.use(express.static("client/build"));
+
+// Start the API server
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
