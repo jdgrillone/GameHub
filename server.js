9103a0f8d2b9a112d525512817669a2f0c44d982
jdgrillone <jdgrillone@gmail.com>
2018-02-16 09:48:39 -0700

    established basic user model

--- server.js
+++ server.js
@@ -1,33 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+// const routes = require("./routes");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// Serve up static assets
+app.use(express.static("client/build"));
+// Add routes, both API and view
+// app.use(routes);
 
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGO_URI || "mongod://localhost/gamehub",
-  {
-    useMongoClient: true
-  }
+  process.env.MONGODB_URI || "mongodb://localhost/gamehub"
 );
 
-// Serve staic assets
-app.use(express.static("client/build"));
-
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
