4d8e87895a46a6a16864dda58679e6ce4c273474
jdgrillone <jdgrillone@gmail.com>
2018-02-16 13:16:54 -0700

    Removed FindAll route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,12 +2,6 @@ const db = require("../models");
 
 // Defining methods for the usersController
 module.exports = {
-    findAll: function(req, res) {
-        db.User
-        .find(req.query)
-        .then(dbModel => res.json(dbMOdel))
-        .catch(err => res.status(422).json(err));
-    },
     create: function(req, res) {
         db.User
         .create(req.body)
--- routes/api/user.js
+++ routes/api/user.js
@@ -3,7 +3,6 @@ const usersController = require("../../controllers/usersController");
 
 // Matches with "/api/users"
 router.route("/")
-    .get(usersController.findAll)
     .post(usersController.create);
 
 module.exports = router;    
