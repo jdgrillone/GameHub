af75f8d7f12655fe5a63b989197702fcd034455c
jdgrillone <jdgrillone@gmail.com>
2018-02-16 16:43:43 -0700

    Added Title component

---
+++ client/src/components/Title/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Title.css";
+
+const Title = () => (
+    <div className="title-panel">
+            Welcome To GameHUB
+    </div>
+);
+
+export default Title;
