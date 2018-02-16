2fa246077d0daf4b1d066dfbfd54468a0a9c91a1
jdgrillone <jdgrillone@gmail.com>
2018-02-16 12:15:56 -0700

    Added dependencies for Passport

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser');
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -19,6 +20,13 @@ mongoose.Promise = global.Promise;
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/gamehub"
 );
+
+// Configuring Passport
+var passport = require('passport');
+var expressSession = require('express-session');
+app.use(expressSession({ secret: 'mySecretKey' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Start the API server
 app.listen(PORT, function () {
