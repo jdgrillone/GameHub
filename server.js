7f1dd5f04e950a8358012d7ba222376b17d499f6
jdgrillone <jdgrillone@gmail.com>
2018-02-23 18:57:03 -0700

    heroku prep

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 const config = require('./config');
 
 // connect to the database and load models
-require('./server/models').connect(config.dbUri);
+require('./server/models').connect(process.env.MONGODB_URI || config.dbUri);
 
 const app = express();
 // tell the app to look for static files in these directories
