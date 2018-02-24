764e1e0dfc4fe757cf9df50f5977cb81859b3c22
jdgrillone <jdgrillone@gmail.com>
2018-02-23 19:41:06 -0700

    Changed express static location for client

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require('./server/models').connect(process.env.MONGODB_URI || config.dbUri);
 const app = express();
 // tell the app to look for static files in these directories
 app.use(express.static('./server/static/'));
-app.use(express.static('./client/dist/'));
+app.use(express.static('./client/build/'));
 // tell the app to parse HTTP body messages
 app.use(bodyParser.urlencoded({ extended: false }));
 // pass the passport middleware
